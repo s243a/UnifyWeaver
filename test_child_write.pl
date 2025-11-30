32f76ce2df7732476e858de05a196cc900b5ce36
John William Creighton (@s243a) <s243a@github.noreply.com>
2025-11-30 01:33:41 -0700

    chore: clean generated artifacts and guard go target

--- test_child_write.pl
+++ test_child_write.pl
@@ -1,4 +1,5 @@
 :- encoding(utf8).
+:- use_module(library(filesex)).
 :- use_module('src/unifyweaver/targets/go_target').
 
 person(alice, 25).
@@ -8,5 +9,6 @@ person(charlie, 45).
 child(X, Age) :- person(X, Age), Age < 18.
 
 test :-
+    make_directory_path('output_test'),
     go_target:compile_predicate_to_go(child/2, [], Code),
-    go_target:write_go_program(Code, 'child.go').
+    go_target:write_go_program(Code, 'output_test/child.go').
--- test_comprehensive_captures.pl
+++ test_comprehensive_captures.pl
@@ -1,7 +1,10 @@
 :- encoding(utf8).
 % Comprehensive capture group tests
 
+:- use_module(library(filesex)).
 :- use_module('src/unifyweaver/targets/go_target').
+
+ensure_output_dir :- make_directory_path('output_test').
 
 % Test 1: Single capture group
 extract_time(Line, Time) :-
@@ -22,27 +25,31 @@ extract_error_msg(Line, Msg) :-
 % Compile tests
 test_single :-
     write('=== Test 1: Single Capture Group ==='), nl,
+    ensure_output_dir,
     go_target:compile_predicate_to_go(extract_time/2, [], Code1),
-    go_target:write_go_program(Code1, 'extract_time.go'),
-    write('extract_time.go written'), nl, nl.
+    go_target:write_go_program(Code1, 'output_test/extract_time.go'),
+    write('output_test/extract_time.go written'), nl, nl.
 
 test_two :-
     write('=== Test 2: Two Capture Groups ==='), nl,
+    ensure_output_dir,
     go_target:compile_predicate_to_go(extract_time_level/3, [], Code2),
-    go_target:write_go_program(Code2, 'extract_time_level.go'),
-    write('extract_time_level.go written'), nl, nl.
+    go_target:write_go_program(Code2, 'output_test/extract_time_level.go'),
+    write('output_test/extract_time_level.go written'), nl, nl.
 
 test_three :-
     write('=== Test 3: Three Capture Groups ==='), nl,
+    ensure_output_dir,
     go_target:compile_predicate_to_go(extract_date_time_level/4, [], Code3),
-    go_target:write_go_program(Code3, 'extract_date_time_level.go'),
-    write('extract_date_time_level.go written'), nl, nl.
+    go_target:write_go_program(Code3, 'output_test/extract_date_time_level.go'),
+    write('output_test/extract_date_time_level.go written'), nl, nl.
 
 test_partial :-
     write('=== Test 4: Partial Match (ERROR only) ==='), nl,
+    ensure_output_dir,
     go_target:compile_predicate_to_go(extract_error_msg/2, [], Code4),
-    go_target:write_go_program(Code4, 'extract_error_msg.go'),
-    write('extract_error_msg.go written'), nl, nl.
+    go_target:write_go_program(Code4, 'output_test/extract_error_msg.go'),
+    write('output_test/extract_error_msg.go written'), nl, nl.
 
 run_all :-
     test_single,
