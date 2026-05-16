f81b79bee0c33c0a2148d12890bb18b5b6be79a5
John William Creighton (s243a) <s243a@users.noreply.github.com>
2026-05-16 15:51:20 -0600

    test(wam-c): cover generated term builtins

--- tests/test_wam_c_target.pl
+++ tests/test_wam_c_target.pl
@@ -925,6 +925,16 @@ test_real_prolog_builtin_executable_smoke :-
     ;   format('[PASS] ~w (gcc unavailable; skipped executable smoke)~n', [Test])
     ).
 
+test_real_prolog_term_builtin_executable_smoke :-
+    Test = 'WAM-C: real Prolog term builtin executable smoke',
+    (   gcc_available
+    ->  (   run_real_prolog_term_builtin_executable_smoke
+        ->  pass(Test)
+        ;   fail_test(Test, 'real Prolog term builtin executable failed')
+        )
+    ;   format('[PASS] ~w (gcc unavailable; skipped executable smoke)~n', [Test])
+    ).
+
 test_real_prolog_multiclause_executable_smoke :-
     Test = 'WAM-C: real Prolog multi-clause executable smoke',
     (   gcc_available
@@ -1285,6 +1295,39 @@ run_real_prolog_builtin_executable_smoke :-
         run_c_smoke_plain(ExePath)
     ->  retractall(user:wam_c_real_builtin(_, _))
     ;   retractall(user:wam_c_real_builtin(_, _)),
+        fail
+    ).
+
+run_real_prolog_term_builtin_executable_smoke :-
+    assertz((user:wam_c_real_term_builtins(T, Label) :-
+        functor(T, Name, Arity),
+        Arity = 2,
+        N is 2,
+        arg(N, T, Arg),
+        atom_concat(Name, '_', Prefix),
+        atom_concat(Prefix, Arg, Label))),
+    (   compile_predicate_to_wam(user:wam_c_real_term_builtins/2, [], WamCode),
+        sub_string(WamCode, _, _, _, 'builtin_call functor/3, 3'),
+        sub_string(WamCode, _, _, _, 'builtin_call arg/3, 3'),
+        sub_string(WamCode, _, _, _, 'builtin_call atom_concat/3, 3'),
+        compile_wam_predicate_to_c(user:wam_c_real_term_builtins/2, WamCode, [], PredCode),
+        compile_wam_runtime_to_c([], RuntimeCode),
+        get_time(Now),
+        Stamp is round(Now * 1000000),
+        wam_c_temp_path('unifyweaver_wam_c_real_term_builtin_smoke', Stamp, TmpBase),
+        format(atom(RuntimePath), '~w_runtime.c', [TmpBase]),
+        format(atom(PredPath), '~w_pred.c', [TmpBase]),
+        format(atom(MainPath), '~w_main.c', [TmpBase]),
+        format(atom(ExePath), '~w_bin', [TmpBase]),
+        write_text_file(RuntimePath, RuntimeCode),
+        format(atom(PredTranslationUnit), '#include "wam_runtime.h"~n~n~w', [PredCode]),
+        write_text_file(PredPath, PredTranslationUnit),
+        wam_c_real_term_builtin_smoke_main(MainCode),
+        write_text_file(MainPath, MainCode),
+        compile_c_smoke_plain(RuntimePath, PredPath, MainPath, ExePath),
+        run_c_smoke_plain(ExePath)
+    ->  retractall(user:wam_c_real_term_builtins(_, _))
+    ;   retractall(user:wam_c_real_term_builtins(_, _)),
         fail
     ).
 
@@ -2735,6 +2778,47 @@ int main(void) {
 }
 ').
 
+wam_c_real_term_builtin_smoke_main(
+'#include "wam_runtime.h"
+
+void setup_wam_c_real_term_builtins_2(WamState* state);
+
+static WamValue make_binary_struct(WamState *state, const char *functor, WamValue left, WamValue right) {
+    WamValue term;
+    term.tag = VAL_STR;
+    term.data.ref_addr = state->H;
+    state->H_array[state->H++] = val_atom(functor);
+    state->H_array[state->H++] = left;
+    state->H_array[state->H++] = right;
+    return term;
+}
+
+int main(void) {
+    WamState state;
+    wam_state_init(&state);
+    setup_wam_c_real_term_builtins_2(&state);
+
+    WamValue pair_term = make_binary_struct(&state, "pair/2", val_atom("left"), val_atom("right"));
+    WamValue ok_args[2] = { pair_term, val_atom("pair_right") };
+    int ok_rc = wam_run_predicate(&state, "wam_c_real_term_builtins/2", ok_args, 2);
+    if (ok_rc != 0 || state.P != WAM_HALT) {
+        wam_free_state(&state);
+        return 10;
+    }
+
+    WamValue unary_term = make_binary_struct(&state, "pair/2", val_atom("left"), val_atom("wrong"));
+    WamValue fail_args[2] = { unary_term, val_atom("pair_right") };
+    int fail_rc = wam_run_predicate(&state, "wam_c_real_term_builtins/2", fail_args, 2);
+    if (fail_rc != WAM_HALT) {
+        wam_free_state(&state);
+        return 20;
+    }
+
+    wam_free_state(&state);
+    return 0;
+}
+').
+
 wam_c_real_multi_smoke_main(
 '#include "wam_runtime.h"
 
@@ -3061,6 +3145,7 @@ run_tests_once :-
     test_kernel_detector_executable_smoke,
     test_streaming_foreign_results_executable_smoke,
     test_real_prolog_builtin_executable_smoke,
+    test_real_prolog_term_builtin_executable_smoke,
     test_real_prolog_multiclause_executable_smoke,
     test_real_prolog_structure_index_executable_smoke,
     test_real_prolog_is_list_executable_smoke,
