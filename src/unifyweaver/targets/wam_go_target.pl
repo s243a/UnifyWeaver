a2b8da6fe7414a64d34a85b45f7acb73f086d02e
John William Creighton <s243a@users.noreply.github.com>
2026-05-19 15:43:30 -0600

    feat(wam-go): add atom codes builtin

--- src/unifyweaver/targets/wam_go_target.pl
+++ src/unifyweaver/targets/wam_go_target.pl
@@ -484,6 +484,9 @@ wam_go_direct_builtin("string_length/2", 2, 'string_length/2').
 wam_go_direct_builtin(char_code/2, 2, 'char_code/2').
 wam_go_direct_builtin('char_code/2', 2, 'char_code/2').
 wam_go_direct_builtin("char_code/2", 2, 'char_code/2').
+wam_go_direct_builtin(atom_codes/2, 2, 'atom_codes/2').
+wam_go_direct_builtin('atom_codes/2', 2, 'atom_codes/2').
+wam_go_direct_builtin("atom_codes/2", 2, 'atom_codes/2').
 wam_go_direct_builtin(atom_string/2, 2, 'atom_string/2').
 wam_go_direct_builtin('atom_string/2', 2, 'atom_string/2').
 wam_go_direct_builtin("atom_string/2", 2, 'atom_string/2').
